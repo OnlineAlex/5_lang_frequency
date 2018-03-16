165a06e41f2c268d5e539f5028408a2b313aa8eb
OnlineAlex <becasualua@gmail.com>
2018-03-16 12:57:02 +0200

    переменовал file на work_file изменил метод решение (с collections.Counter())

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,52 +1,50 @@
+import sys
 import string
 import re
+import collections
+
 
 def load_data(filepath):
-    with open(filepath, 'r') as file:
-        file = re.sub("\s*\n\s*", ' ', file.read())
-        return file
+    with open(filepath, 'r') as work_file:
+        return work_file.read()
 
 
 def remove_punctuation(work_text):
+    work_text = re.sub("\s*\n|\xa0|—\s*", ' ', work_text)
     table = work_text.maketrans("", "", string.punctuation)
     clean_text = work_text.translate(table)
     return clean_text
 
 
-def get_unique_words(words_text):
-    set_words = set(word for word in words_text.split(' '))
-    if '' in set_words:
-        set_words.remove('')
-    tuple_words = tuple(set_words)
-    return tuple_words
-
-
 def get_most_frequent_words(text):
     text = text.lower()
     clean_text = remove_punctuation(text)
-    unique_words = get_unique_words(clean_text)
-    list_all_words = clean_text.split(' ')
+    list_all_words = clean_text.split()
+    words_stat = collections.Counter()
+    for word in list_all_words:
+        words_stat[word] += 1
 
-    words_stat = {}
-    for word in unique_words:
-        word_repeat = list_all_words.count(word)
-        words_stat[word] = word_repeat
+    return words_stat.most_common(10)
 
-    words_stat = sorted(words_stat.items(), key=lambda value: value[1])
-    return words_stat
+
+def print_frequent_words(top_freq_words):
+    place = 1
+    for word, frequent in top_freq_words:
+        print('{0:>2}: "{1}" — {2}'.format(place, word, frequent))
+        place += 1
 
 
 if __name__ == '__main__':
-    while True:
-        try:
-            filepath = input('Напишите путь к файлу в формате .txt \n')
-            text_file = load_data(filepath)
-        except FileNotFoundError:
-            print('Ошибка! Система не нашла такой файл.')
-            print('Пробуйте указать полный путь к файлу.\n')
-        else:
-            print('Файл принял, начинаю анализ...')
-            break
-    frequent_stat = get_most_frequent_words(text_file)
-    for place in range(1, 11):
-        print('{0}: "{1}" — {2}'.format(place, frequent_stat[-place][0], frequent_stat[-place][1]))
+    try:
+        link_file = sys.argv[1]
+        text_file = load_data(link_file)
+    except IndexError:
+        print('Ошибка! Вы не указали путь к файлу.')
+        print('Сработает, если написать "python lang_frequency.py <путь к файлу>"')
+    except FileNotFoundError:
+        print('Ошибка! Система не нашла такой файл.')
+    except ValueError:
+        print('Ошибка. Файл должен быть в формате .txt')
+    else:
+        frequent_stat = get_most_frequent_words(text_file)
+        print_frequent_words(frequent_stat)
